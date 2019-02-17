fe709cdff4214286980bce996268fc9e3d19f97d
RakhmanovAV <Rav3110@gmail.com>
2019-02-17 22:19:14 +0300

    6 task

--- 6.js
+++ 6.js
@@ -1,26 +1,27 @@
 var s = 'aba';
-var n = 50;
+var n = 89;
 var b ='';
 var target = 'a'
 function myFunction(s,n, target){
 var charMap = 0
+
 do {
     b += s
-} while(b.length<10)
+} while(b.length<n)
+
 b=b.substr(0,n);
+
 for (let char of b){
     if (char == target) {
         charMap = charMap+1
     }
-    
 }
-console.log(charMap[target]);
-console.log(charMap)
-return charMap[target]
+
+return charMap
 }
 
 
 
 
 
-myFunction(s,n, target)
+console.log(myFunction(s,n,target))
