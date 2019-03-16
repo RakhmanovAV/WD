d153a56ed249191177cd58ed45d70dd392e15f5f
RakhmanovAV <45531517+RakhmanovAV@users.noreply.github.com>
2019-03-16 23:34:17 +0300

    Update 11.js
    With context and arguments

--- 11.js
+++ 11.js
@@ -1,16 +1,32 @@
-function bind(func, a) {
-    return function(e) { 
-       return func.call(null, e, a);
+
+function bind(func, context, atguments, arg) {
+
+    return function() { 
+var args = Array.from(arguments)
+args.push(arg);
+       return func.apply(context, args);
+
     };
+
   } 
 
-function sum(a, b){
-    return a+b;
+
+function sum(a, b, c){
+console.log(1 + '=' + this)
+console.log(2 + '=' +a) 
+console.log(3 + '=' +b)
+console.log(4 + '=' +c)
+    return a+b+c;
+
 }
 
+
 // const partsum = sum.bind(null, 1)
+
 // console.log(partsum(4))
 
-const f = bind(sum, 10);
-console.log(f(5))
+
+const f = bind(sum, 'context', arguments, 22);
+
+console.log(f(5,9))
 
